33989778c020fd1062b4f2e2c0fc1589eccfd01f
Ema <sedyte.ema@gmail.com>
2024-04-08 23:14:40 +0200

    Extra Treatments css

--- src/pages/ExtraTreatments/MassageAndScrub.jsx
+++ src/pages/ExtraTreatments/MassageAndScrub.jsx
@@ -8,7 +8,7 @@ function MassageAndScrub() {
             <img src={MassageAndScrubImage} id="massageAndScrub" alt="ScrubBrush" />
             <h2>Massage and Scrub</h2>
             <div className="text-box">
-                <p>Experience pure bliss with our Massage and Scrub treatment at Cloud Spa. 
+                <p> Experience pure bliss with our Massage and Scrub treatment at Cloud Spa. 
                     Relax as skilled hands gently knead away tension with a soothing massage, 
                     followed by an invigorating scrub to exfoliate and renew your skin. 
                     Feel the stress melt away and emerge with a radiant glow, ready to embrace 
