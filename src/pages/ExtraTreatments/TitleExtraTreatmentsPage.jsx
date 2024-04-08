760cda863e964c12f99ae74e123fb47cbd48abe9
Ema <sedyte.ema@gmail.com>
2024-04-08 23:19:09 +0200

    fixed extra treatments title

--- src/pages/ExtraTreatments/TitleExtraTreatmentsPage.jsx
+++ src/pages/ExtraTreatments/TitleExtraTreatmentsPage.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ClassicMassageImage from '../public/assets/Pictures/classicMassage.jpg';
 import '../public/assets/styles/ExtraTreatmentsPage.css';
 
-function HeaderExstraTreatmentsPage() {
+function TitleExtraTreatmentsPage() {
     return (
-        <div>
-             {/* Main header of the page */}
+        <div className="extra-treatments-title">
+             {/* Main title of the page */}
              <h1>Boost Your Wellbeing with our Extra Treatments</h1>
         </div>
     );
 }
 
-export default HeaderExstraTreatmentsPage;
+export default TitleExtraTreatmentsPage;
