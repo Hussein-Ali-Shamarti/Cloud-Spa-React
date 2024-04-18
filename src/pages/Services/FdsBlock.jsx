92b2f1ded3f2d0d10871ba7436b25faabf8fc236
nordwik <126633118+nordwik@users.noreply.github.com>
2024-04-18 13:59:17 +0200

    Update FDS service to correctly link to Booking

--- src/pages/Services/FdsBlock.jsx
+++ src/pages/Services/FdsBlock.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import fullDaySpa from "../../Pictures/fullDaySpa.jpg";
 import "../../styles/Services/FdsBlock.css";
 import { useNavigate } from "react-router-dom";
+import { SelectedServiceContext } from "../../ServicesContext";
 
 const FdsBlock = () => {
   const navigate = useNavigate(); // Bruk useNavigate hook for å få tilgang til navigere funksjonen
-
-  const handleBookNow = () => {
-    navigate("/Booking")
+  
+  const { setSelectedService } = useContext(SelectedServiceContext); // Tillater at man kan overføre inputs til andre filer
+  const FDStitle = "Full Day Spa Retreat"; // Gjør tittelen til en constant som kan sendes til andre filer
+  
+  const handleBookNow = (service) => {
+    setSelectedService(service);
+    navigate("/Booking");
   };
 
   return (
@@ -15,7 +20,7 @@ const FdsBlock = () => {
       <img src={fullDaySpa} alt="Full day Spa Retreat" />
 
       <div className="FDS-info">
-        <h4 className="FDS-title">Full Day Spa Retreat</h4>
+        <h4 className="FDS-title" >{FDStitle}</h4>
         {"\n"}
 
         <p className="FDS-text-box">
@@ -25,7 +30,11 @@ const FdsBlock = () => {
         {"\n"}
         <p className="FDS-price">Just for 1000kr</p>
         {"\n"}
-        <button className="FDS-book-btn" onClick={() =>handleBookNow()}>Book now</button>
+        <button className="FDS-book-btn" 
+          onClick={() =>handleBookNow(FDStitle)}
+          >
+            Book now
+            </button>
       </div>
     </div>
   );
