b112a3d32ee96aa60dee7661e821afba915045f4
MarziyehKhaleghi <marziyeh.khaleghi92@gmail.com>
2024-05-07 20:09:18 +0200

    pushing

--- src/pages/Booking2/Calendar.jsx
+++ src/pages/Booking2/Calendar.jsx
@@ -2,48 +2,58 @@ import React, {useState, useEffect} from "react";
 import "../../styles/Booking2/Calendar.css";
 
 
-
 const Calendar = () => {
   
   const [currentDate2, setCurrentDate]= useState("");
   const [days2, setDays] = useState([]);
   const [currentMonth2, setCurrentMonth] = useState(new Date().getMonth());
   const [currentYear2, setCurrentYear] = useState(new Date().getFullYear());
-  const [today, setToday]= useState(new Date());
+  const [today, setToday] = useState(new Date());
+  const [prevMOnthDays, setPrevMonthDays] = useState([]);
+  const [nextMOnthDays, setNextMonthDays] = useState([]);
+  
 
   const WeekDays2 = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-  const adjustedWeekDays2 = [...WeekDays2];
+  
   
   useEffect(() => {
+   
     updateCalendar2();
   }, [currentMonth2, currentYear2]);
 
   const updateCalendar2 = () => {
     const date2 = new Date(currentYear2, currentMonth2, 1);
-    let firstDayOfWeek = date2.getDay();
-    if (firstDayOfWeek === 0) {
-      firstDayOfWeek = 6; // Søndag, juster til lørdag (6)
+    let firstDayOfMOnth = date2.getDay();
+    if (firstDayOfMOnth=== 0) {
+      firstDayOfMOnth = 6; // Søndag, juster til lørdag (6)
       } else {
-      firstDayOfWeek--; // Juster tilsvarende én mindre for alle andre dager
+        firstDayOfMOnth--; // Juster tilsvarende én mindre for alle andre dager
       }
 
     const lastDateOfMonth = new Date(currentYear2, currentMonth2 + 1, 0).getDate();
     const daysArray = [];
+    const PrevMONthDays = [];
+    const NextMOnthDays = [];
 
     const lastDateOfPrevMonth = new Date(currentYear2, currentMonth2, 0).getDate();
-    for (let i = firstDayOfWeek - 1; i >= 0; i--) {
-      daysArray.push(lastDateOfPrevMonth - i);
+    for (let i = firstDayOfMOnth - 1; i >= 0; i--) {
+      PrevMONthDays.push(lastDateOfPrevMonth - i);
     }
     for (let i = 1; i <= lastDateOfMonth; i++) {
       daysArray.push(i);
     }
-    const remainingDays = 7 - (daysArray.length % 7);
+    const remainingDays = 7 - (PrevMONthDays.length + daysArray.length % 7);
     if (remainingDays < 7) {
       for (let i = 1; i <= remainingDays; i++) {
-        daysArray.push(i);
+        NextMOnthDays.push(i);
       }
     }
+    const current = new Date();
+    current.setHours(0, 0, 0, 0); // Nullstill tidspunktet for en nøyaktig dato sammenligning
+    setToday(current); 
+    setPrevMonthDays(PrevMONthDays);
     setDays(daysArray);
+    setNextMonthDays(NextMOnthDays);
     setCurrentDate(`${new Date(currentYear2, currentMonth2).toLocaleString('default', { month: 'long' })} ${currentYear2}`);
   };
 
@@ -55,6 +65,7 @@ const Calendar = () => {
       setCurrentMonth(currentMonth2 + 1);
     }
   };
+  
 
   const goToPrevMonth2 = () => {
     if (currentMonth2 === 0) {
@@ -66,7 +77,6 @@ const Calendar = () => {
   };
 
   
-
 
 
     return(
@@ -83,14 +93,26 @@ const Calendar = () => {
           <div className="Calendar2">
             
             <ul className="weeks2">   
-            {adjustedWeekDays2.map(day => (
+            {WeekDays2.map(day => (
               <li key={day}>{day}</li>
             ))}
             </ul>
             <ul className="days2">
-            {days2.map((day, index) => (
-              <li key={index}>{day}</li>
-              ))}  
+                {prevMOnthDays.map((day, index) => (
+                <li key={index} className="past">
+                  {day}
+                </li> 
+                ))}
+               {days2.map((day , index) => {
+                const dayDate = new Date(currentYear2, currentMonth2, day);
+                let dayState = dayDate < today ? 'past' : 'future';
+                return <li key={index} className={dayState}>{day}</li>
+                })}
+                {nextMOnthDays.map((day, index) => (
+                <li key={index} className="future">
+                  {day}
+                </li> 
+                ))}
                 
             </ul>
           </div>
