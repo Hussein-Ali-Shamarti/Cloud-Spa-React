61ff83179345b88f507674bc7049fc6e95f94f4c
Hussein Ali Al-Shamarti <hussein.a220@icloud.com>
2024-05-13 11:16:22 +0200

    Update Calendar.jsx

--- src/pages/Booking2/Calendar.jsx
+++ src/pages/Booking2/Calendar.jsx
@@ -1,12 +1,10 @@
-import React, {useState, useEffect, useContext} from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../styles/Booking2/Calendar.css";
 import { SelectedServiceContext } from "../../ServicesContext.js";
 
-
 const Calendar = () => {
-  
-  const [currentDate2, setCurrentDate]= useState("");
+  const [currentDate2, setCurrentDate] = useState("");
   const [days2, setDays] = useState([]);
   const [currentMonth2, setCurrentMonth] = useState(new Date().getMonth());
   const [currentYear2, setCurrentYear] = useState(new Date().getFullYear());
@@ -15,36 +13,42 @@ const Calendar = () => {
   const [nextMOnthDays, setNextMonthDays] = useState([]);
   const { selectedDate, setSelectedDate } = useContext(SelectedServiceContext);
 
-  const WeekDays2 = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-  
-  
+  const WeekDays2 = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
   useEffect(() => {
-   
     updateCalendar2();
   }, [currentMonth2, currentYear2]);
 
   const updateCalendar2 = () => {
     const date2 = new Date(currentYear2, currentMonth2, 1);
     let firstDayOfMOnth = date2.getDay();
-    if (firstDayOfMOnth=== 0) {
+    if (firstDayOfMOnth === 0) {
       firstDayOfMOnth = 6; // Søndag, juster til lørdag (6)
-      } else {
-        firstDayOfMOnth--; // Juster tilsvarende én mindre for alle andre dager
-      }
+    } else {
+      firstDayOfMOnth--; // Juster tilsvarende én mindre for alle andre dager
+    }
 
-    const lastDateOfMonth = new Date(currentYear2, currentMonth2 + 1, 0).getDate();
+    const lastDateOfMonth = new Date(
+      currentYear2,
+      currentMonth2 + 1,
+      0
+    ).getDate();
     const daysArray = [];
     const PrevMONthDays = [];
     const NextMOnthDays = [];
 
-    const lastDateOfPrevMonth = new Date(currentYear2, currentMonth2, 0).getDate();
+    const lastDateOfPrevMonth = new Date(
+      currentYear2,
+      currentMonth2,
+      0
+    ).getDate();
     for (let i = firstDayOfMOnth - 1; i >= 0; i--) {
       PrevMONthDays.push(lastDateOfPrevMonth - i);
     }
     for (let i = 1; i <= lastDateOfMonth; i++) {
       daysArray.push(i);
     }
-    const remainingDays = 7 - (PrevMONthDays.length + daysArray.length) % 7;
+    const remainingDays = 7 - ((PrevMONthDays.length + daysArray.length) % 7);
     if (remainingDays < 7) {
       for (let i = 1; i <= remainingDays; i++) {
         NextMOnthDays.push(i);
@@ -52,11 +56,15 @@ const Calendar = () => {
     }
     const current = new Date();
     current.setHours(0, 0, 0, 0); // Nullstill tidspunktet for en nøyaktig dato sammenligning
-    setToday(current); 
+    setToday(current);
     setPrevMonthDays(PrevMONthDays);
     setDays(daysArray);
     setNextMonthDays(NextMOnthDays);
-    setCurrentDate(`${new Date(currentYear2, currentMonth2).toLocaleString('default', { month: 'long' })} ${currentYear2}`);
+    setCurrentDate(
+      `${new Date(currentYear2, currentMonth2).toLocaleString("default", {
+        month: "long"
+      })} ${currentYear2}`
+    );
   };
 
   const goToNextMonth2 = () => {
@@ -67,7 +75,6 @@ const Calendar = () => {
       setCurrentMonth(currentMonth2 + 1);
     }
   };
-  
 
   const goToPrevMonth2 = () => {
     if (currentMonth2 === 0) {
@@ -86,64 +93,56 @@ const Calendar = () => {
     navigate("/Booking3");
   };
 
-
-    return(
-       
-       <div className="Calendar-Container">
-       <div className="wrapper-Calendar">
-          <div className="header2">
-            <h5 className="current-date2">{currentDate2}</h5>
-            <div className="icons">
-                <span className="before-symbol" onClick={goToPrevMonth2}> &lt; </span>
-                <span className="after-symbol" onClick={goToNextMonth2}> &gt; </span>
-            </div>
+  return (
+    <div className="Calendar-Container">
+      <div className="wrapper-Calendar">
+        <div className="header2">
+          <h5 className="current-date2">{currentDate2}</h5>
+          <div className="icons">
+            <span className="before-symbol" onClick={goToPrevMonth2}>
+              {" "}
+              &lt;{" "}
+            </span>
+            <span className="after-symbol" onClick={goToNextMonth2}>
+              {" "}
+              &gt;{" "}
+            </span>
           </div>
-          <div className="Calendar2">
-            
-            <ul className="weeks2">   
-            {WeekDays2.map(day => (
+        </div>
+        <div className="Calendar2">
+          <ul className="weeks2">
+            {WeekDays2.map((day) => (
               <li key={day}>{day}</li>
             ))}
-            </ul>
-            <ul className="days2">
-<<<<<<< HEAD
-                  {prevMOnthDays.map((day, index) => (
-                    <li key={index} className='inActive'> {day} </li> 
-                  ))}
+          </ul>
+          <ul className="days2">
+            {prevMOnthDays.map((day, index) => (
+              <li key={index} className="inActive">
+                {" "}
+                {day}{" "}
+              </li>
+            ))}
 
-                  {days2.map((day , index) => {
-                    const dayDate = new Date(currentYear2, currentMonth2, day);
-                    let dayState = dayDate < today ? 'inActive' : 'Active';
-                    return <li key={index} className={dayState}>{day}</li>
-                  })}
-
-                  {nextMOnthDays.map((day, index) => (
-                    <li key={index} className='inActive'> {day} </li> 
-                  ))}
-=======
-                {prevMOnthDays.map((day, index) => (
-                <li key={index} className="past">
+            {days2.map((day, index) => {
+              const dayDate = new Date(currentYear2, currentMonth2, day);
+              let dayState = dayDate < today ? "inActive" : "Active";
+              return (
+                <li key={index} className={dayState}>
                   {day}
-                </li> 
-                ))}
-               {days2.map((day , index) => {
-                const dayDate = new Date(currentYear2, currentMonth2, day);
-                let dayState = dayDate < today ? 'past' : 'future';
-                return  <li key={index} className={dayState} onClick={() => handleDateClick(currentYear2, currentMonth2, day)}>{day}</li>
-                })}
-                {nextMOnthDays.map((day, index) => (
-                <li key={index} className="future">
-                  {day}
-                </li> 
-                ))}
->>>>>>> 05691713b18af2c4e76b1f34565a52b78917d836
-                
-            </ul>
-          </div>
+                </li>
+              );
+            })}
 
-       </div>
-       </div>
-       
-    );
+            {nextMOnthDays.map((day, index) => (
+              <li key={index} className="inActive">
+                {" "}
+                {day}{" "}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Calendar;
