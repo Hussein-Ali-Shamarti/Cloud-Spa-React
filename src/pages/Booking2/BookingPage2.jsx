4a4bd74e4ba0e9c7fe71f1b9d6b85428b2366032
MarziyehKhaleghi <marziyeh.khaleghi92@gmail.com>
2024-05-14 23:02:59 +0200

    pushing

--- src/pages/Booking2/BookingPage2.jsx
+++ src/pages/Booking2/BookingPage2.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import booking2 from "../../Pictures/booking2.jpg";
 import "../../styles/Booking2/Pic.css";
-
+import { useNavigate, Link } from "react-router-dom";
 import Layout from "../../components/layout";
 import Calendar from "./Calendar";
 import BookingPath from "./BookingPath";
 import QuantityPers from "./QuantityPers";
+import "../../styles/BookingButtons.css";
 
 const BookingPage2 = () => {
+
+  const navigate = useNavigate();
+  const handleNext2 = () => {
+    navigate("/Booking3");
+  };
 
     return(
       <div className="Date">
@@ -22,6 +28,16 @@ const BookingPage2 = () => {
           <BookingPath/>
           <Calendar/>
           <QuantityPers/>
+
+          <div className="booking-buttons">
+          <Link to="/" className="booking-cancel-button">
+            Cancel
+          </Link>
+          <button className="booking-next-button" onClick={handleNext2}>
+             Next
+          </button>
+          
+        </div>
           
         </div>
         
--- src/pages/Booking2/QuantityPers.jsx
+++ src/pages/Booking2/QuantityPers.jsx
@@ -1,12 +1,33 @@
-import react from "react";
+import React, { useState } from "react";
+import "../../styles/Booking2/QuantityPers.css";
+import iconPerson from "../../Pictures/iconPerson.png";
 
 const QuantityPers = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
 
   return(
     <div className= "Quantity-Container">
-        <div className="Quantity-buttoms">
-            
+        <div className="age-label">
+          <img src={iconPerson} alt="PersonIcon" className= "personIcon" />
+          Age 16+
         </div>
+        <div className="counter-controls">
+          <button onClick={handleDecrement} disabled={count === 0}>-</button>
+          <span>{count}</span>
+          <button onClick={handleIncrement}>+</button>
+        </div>
+            
+        
     </div>
   );
 
